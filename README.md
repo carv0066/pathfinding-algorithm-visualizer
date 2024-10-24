507f3610547cb4c197e1f576cc156126622e2e18
Carlos Carvajal <carv0066@algonquinlive.com>
2024-10-23 22:23:26 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Pathfinding  Algorithm  Visualizer
-
-This project generates a random maze and it allows you to find the correct path using the breadth Breadth-First Search (BFS) and Depth-First Search (DFS).
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MazeGrid from './MazeGrid'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<MazeGrid />
-	</React.StrictMode>
-)
